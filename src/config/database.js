51d50d3b854052581e5b9e4d3aebae7893c32e1e
Natanael Pereira <natanaelpereira745@gmail.com>
2025-07-01 09:55:47 -0300

    API + PostgreSQL

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ module.exports = {
   dialect: "postgres",
   host: "localhost",
   username: "postgres",
-  password: "secret",
+  password: "123",
   database: "teste-dominando-nodejs",
   define: {
     timestamp: true, // cria duas colunas: createdAt e updatedAt
-    underscored: true,
+    underscored: true, //nomeclatura _ (não camelcase)
     underscoredAll: true,
   },
 };
